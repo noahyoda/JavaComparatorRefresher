85f1180e6ec3d5ef9e3997f5c660102c5dd1062a
noahyoda <67946197+noahyoda@users.noreply.github.com>
2021-06-06 18:40:38 -0600

    Add files via upload

---
+++ SubStrings.java
@@ -0,0 +1,42 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class SubStrings {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		int k = in.nextInt();
+		
+		ArrayList<String> lines = new ArrayList<>();
+		
+		for(int i = 0; i <= s.length() - k; i++) {
+			String temp = s.substring(i, i + k);
+			lines.add(temp);
+		}
+		
+		Collections.sort(lines, new OrderLexigraph());
+		System.out.println(lines.get(0) + "\n" + lines.get(lines.size()-1));
+		
+	}
+	
+	public static class OrderLexigraph implements Comparator<String> {
+
+		@Override
+		public int compare(String o1, String o2) {
+			if(o1.charAt(0) > o2.charAt(0))
+				return 1;
+			else if (o1.charAt(0) < o2.charAt(0))
+				return -1;
+			else
+				return 0;
+		}
+		
+	}
+
+}
